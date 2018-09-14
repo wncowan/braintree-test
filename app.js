227e68ffd200e7ea48c564007a2e62a40c69471c
raebex <r.young1025@gmail.com>
2018-09-14 14:54:00 -0500

    Add error handling + eslint

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
-const braintree = require("braintree");
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const flash = require('connect-flash');
-
 const routes = require('./routes/index');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(flash());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+// error handlers
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+
+  err.status = 404;
+  next(err);
+});
+
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
