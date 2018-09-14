fa3bddb6a8fad330813a1e6217d725b404d49059
raebex <r.young1025@gmail.com>
2018-09-14 14:53:06 -0500

    Show error page

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,45 @@
-'use strict';
+const express = require('express');
+const braintree = require('braintree');
+const gateway = require('../lib/gateway');
 
-var express = require('express');
-var braintree = require('braintree');
-var router = express.Router();
-var gateway = require('../lib/gateway');
+const router = express.Router();
 
-var TRANSACTION_SUCCESS_STATUSES = [
+const TRANSACTION_SUCCESS_STATUSES = [
   braintree.Transaction.Status.Authorizing,
   braintree.Transaction.Status.Authorized,
   braintree.Transaction.Status.Settled,
   braintree.Transaction.Status.Settling,
   braintree.Transaction.Status.SettlementConfirmed,
   braintree.Transaction.Status.SettlementPending,
-  braintree.Transaction.Status.SubmittedForSettlement
+  braintree.Transaction.Status.SubmittedForSettlement,
 ];
 
 function formatErrors(errors) {
-  var formattedErrors = '';
+  let formattedErrors = '';
 
   for (var i in errors) { // eslint-disable-line no-inner-declarations, vars-on-top
     if (errors.hasOwnProperty(i)) {
-      formattedErrors += 'Error: ' + errors[i].code + ': ' + errors[i].message + '\n';
+      formattedErrors += `Error: ${errors[i].code}: ${errors[i].message}\n`;
     }
   }
   return formattedErrors;
 }
 
 function createResultObject(transaction) {
-  var result;
-  var status = transaction.status;
+  let result;
+  const status = transaction.status;
 
   if (TRANSACTION_SUCCESS_STATUSES.indexOf(status) !== -1) {
     result = {
       header: 'Sweet Success!',
       icon: 'success',
-      message: 'Your test transaction has been successfully processed. See the Braintree API response and try again.'
+      message: 'Your test transaction has been successfully processed. See the Braintree API response and try again.',
     };
   } else {
     result = {
       header: 'Transaction Failed',
       icon: 'fail',
-      message: 'Your test transaction has a status of ' + status + '. See the Braintree API response and try again.'
+      message: `Your test transaction has a status of ${status}. See the Braintree API response and try again.`,
     };
   }
 
@@ -53,39 +52,38 @@ router.get('/', function (req, res) {
 
 router.get('/checkouts/new', function (req, res) {
   gateway.clientToken.generate({}, function (err, response) {
-    res.render('checkouts/new', {title: 'New Checkout', clientToken: response.clientToken});
+    res.render('checkouts/new', { clientToken: response.clientToken, msg: '' });
   });
 });
 
 router.post("/checkout", function (req, res) {
-  let amount = "10";
-  let nonceFromTheClient = req.body.payment_method_nonce;
+  const amount = "10";
+  const nonceFromTheClient = req.body.payment_method_nonce;
 
   // Create a transaction
   gateway.transaction.sale({
     amount: amount,
     paymentMethodNonce: nonceFromTheClient,
     options: {
-      submitForSettlement: true
-    }
+      submitForSettlement: true,
+    },
   }, function (err, result) {
     if (result.success || result.transaction) {
-      res.redirect('checkouts/' + result.transaction.id);
+      res.redirect(`checkouts/${result.transaction.id}`);
     } else {
-      transactionErrors = result.errors.deepErrors();
-      req.flash('error', {msg: formatErrors(transactionErrors)});
-      res.redirect('checkouts/new');
+      var transactionErrors = result.errors.deepErrors();
+      res.render('checkouts/error', { msg: formatErrors(transactionErrors) });
     }
   });
 });
 
 router.get('/checkouts/:id', function (req, res) {
-  var result;
-  var transactionId = req.params.id;
+  const transactionId = req.params.id;
+  let result;
 
   gateway.transaction.find(transactionId, function (err, transaction) {
     result = createResultObject(transaction);
-    res.render('checkouts/show', {title: 'Success', transaction: transaction, result: result});
+    res.render('checkouts/show', { transaction, result });
   });
 });
 
